91d4d3191055b4674f6f10d8092a3f2b6772dc4c
Nolan Fahey <njfahey1@gmail.com>
2019-09-09 17:13:20 -0500

    Init gatsby-starter within existing project
    Add gatsby-starter website files

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,5 +5,42 @@
  */
 
 module.exports = {
-  /* Your site config here */
+  siteMetadata: {
+    title: `Nolan Fahey`,
+    description: `Nolan Fahey's personal website and blog`,
+    author: `Nolan Fahey`,
+  },
+  plugins: [
+    {
+      resolve: `gatsby-plugin-page-creator`,
+      options: {
+        path: `${__dirname}/src/components/pages`,
+      },
+    },
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Nolan Fahey`,
+        short_name: `NF`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    // `gatsby-plugin-offline`,
+  ],
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,21 @@
 import React from 'react';
-import CustomHeader from '../component/common/CustomHeader';
+import { Link } from 'gatsby';
 
-const App = () => {
-  return <CustomHeader text="Hello world" />;
-};
+import Layout from '../components/layout';
+import Image from '../components/image';
+import SEO from '../components/seo';
 
-export default App;
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Home" />
+    <h1>Hi people</h1>
+    <p>Welcome to your new Gatsby site.</p>
+    <p>Now go build something great.</p>
+    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+      <Image />
+    </div>
+    <Link to="/page-2/">Go to page 2</Link>
+  </Layout>
+);
+
+export default IndexPage;
